d8a5f85322978a24630f212066e5f397cbf674d9
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-02-05 15:34:26 +0300

    Initial Commit

---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Sezer on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
